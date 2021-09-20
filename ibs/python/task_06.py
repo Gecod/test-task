684b752e81ddbfe510f0557317e4b3385debfac7
Gecod <56275019+Gecod@users.noreply.github.com>
2021-09-20 22:49:32 +0400

    Create task_06.py

---
+++ ibs/python/task_06.py
@@ -0,0 +1,24 @@
+class Animal:
+    def voice(self):
+        pass
+
+class Cat(Animal):
+    def voice(self):
+        print('Meow')
+
+class Dog(Animal):
+    def voice(self):
+        print('Woof')
+
+class Cow(Animal):
+    def voice(self):
+        print('Moo')
+
+tom = Cat()
+tom.voice()
+
+rex = Dog()
+rex.voice()
+
+betty = Cow()
+betty.voice()
