68f458d96a1e4daba0d4a87ea7b548d1a98f4229
Gecod <56275019+Gecod@users.noreply.github.com>
2021-09-20 23:51:52 +0400

    Update task_06.py

--- ibs/python/task_06.py
+++ ibs/python/task_06.py
@@ -2,17 +2,21 @@ class Animal:
     def voice(self):
         pass
 
+
 class Cat(Animal):
     def voice(self):
         print('Meow')
+
 
 class Dog(Animal):
     def voice(self):
         print('Woof')
 
+
 class Cow(Animal):
     def voice(self):
         print('Moo')
+
 
 tom = Cat()
 tom.voice()
