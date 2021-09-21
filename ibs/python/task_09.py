cabc9d80b766a92777af2ab0c1d95893c1becb04
Gecod <56275019+Gecod@users.noreply.github.com>
2021-09-21 12:02:17 +0400

    Create task_09.py

---
+++ ibs/python/task_09.py
@@ -0,0 +1,19 @@
+import threading
+import time
+
+
+def print_rev_num(num):
+    for i in range(num, 0, -1):
+        print("Current thread: ", format(threading.currentThread().getName()))
+        print(i)
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    count_from = 10
+
+    t1 = threading.Thread(target=print_rev_num, args=(count_from,))
+    t2 = threading.Thread(target=print_rev_num, args=(count_from,))
+
+    t1.start()
+    t2.start()
